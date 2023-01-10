ef4f68546817c6105091fa0a36a67a407c01ff23
JonginWon <jonginwon1203@gmail.com>
2023-01-10 16:37:54 +0900

    Add: Sign-Up form

--- ch1/front/.next/static/webpack/webpack.caa82b1d502858b3.hot-update.js
+++ ch1/front/.next/static/webpack/webpack.caa82b1d502858b3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "49d9eeda85431238"; }
+/******/ 	__webpack_require__.h = function() { return "0ea9292d7b036d1a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- ch1/front/components/LoginForm.js
+++ ch1/front/components/LoginForm.js
@@ -2,22 +2,16 @@ import React, { useCallback, useState } from "react";
 import { Button, Form, Input } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import useInput from "../hooks/useInput";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
 const LoginForm = ({ setIsLoggedIn }) => {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onChangeId = useCallback((e) => {
-    setId(e.target.value);
-  }, []);
-
-  const onChangePassword = useCallback((e) => {
-    setPassword(e.target.value);
-  }, []);
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
@@ -51,6 +45,10 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </ButtonWrapper>
     </Form>
   );
+};
+
+LoginForm.prototype = {
+  setIsLoggedIn: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
